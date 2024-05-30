7008bb82557ff12065291e9d0d3f1592ae74493d
jlh7 <j.lh.t@outlook.com>
2024-05-30 20:12:11 +0700

    chore: Update update-profile.sh to set PS1 color for root user

--- SettingUp/update-profile.sh
+++ SettingUp/update-profile.sh
@@ -44,7 +44,7 @@ fi
 echo "Updating profile..."
 echo "- Set color..."
 echo "PS1='\[\033[01;32m\]\u\[\033[01;37m\]@\[\033[01;33m\]\h\[\033[01;31m\]:\[\033[01;36m\] \w\n\[\033[00m\]\$ '" >>"/home/$_user/.bashrc"
-echo "PS1='\[\033[01;33m\]\h\[\033[01;31m\]:\[\033[01;36m\] \w\n\[\033[00m\]\$ '" >>"~/.bashrc"
+echo "PS1='\[\033[01;33m\]\h\[\033[01;31m\]:\[\033[01;36m\] \w\n\[\033[00m\]\$ '" >>"/root/.bashrc"
 source ~/.bashrc
 
 echo "- Sudo without password..."
