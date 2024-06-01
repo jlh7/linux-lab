20073c679c32d2b4845b24b636d62678e0525709
jlh7 <j.lh.t@outlook.com>
2024-06-01 11:42:46 +0700

    chore: Update network configuration and remove unnecessary files

--- SettingUp/Network/setup.sh
+++ SettingUp/Network/setup.sh
@@ -49,7 +49,7 @@ hostnamectl set-hostname $_host
 echo "- Setting network from file ..."
 cat ./hosts.cfg >/etc/hosts
 rm -rf /etc/netplan/*.yaml
-cat ./01-netcfg.yaml >/etc/netplan/00-netcfg.yaml
+cp ./00-netcfg.yaml /etc/netplan
 chmod 600 /etc/netplan/00-netcfg.yaml
 netplan apply
 echo "------------------------------------------ DONE ------------------------------------------"
